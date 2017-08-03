cd73a42bf892c6c5e2270035aa35c99fcf1b29bc
Abdurrahman YILMAZ <15090111061@ybu.edu.tr>
2017-08-03 12:45:26 +0300

    Update lumba.sh

--- lumba.sh
+++ lumba.sh
@@ -3,12 +3,12 @@ IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install xrdp -y && sudo apt-get install xfce4 -y && echo xfce4-session >~/.xsession && sudo apt-get install firefox -y && clear && sudo service xrdp restart
 echo 
 echo !!!!!!!!!!!!!!!!!!!!!!!!
-echo !! Kurulum tamamlandı.!!
+echo !! Kurulum tamamlandi.!!
 echo !!!!!!!!!!!!!!!!!!!!!!!!
 echo
 echo '           Kurulum Bilgileri'
 echo '###########################################'
 echo "   Sunucu adresi: $IP"
-echo 'Kullanıcı adı ve şifreniz putty e giriş yaptığınız bilgilerle aynı.'
-echo 'Bir sorun oluşursa kendi başınıza çözmeye çalışın.'
+echo 'Kullanici adi ve sifreniz putty e giris yaptiginiz bilgilerle ayni.'
+echo 'Bir sorun olusursa kendi basiniza cozmeye calisin.'
 echo '###########################################'
