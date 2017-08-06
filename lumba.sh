c7b84237178e300b8b0fd2b064e98d6c6210035a
Abdurrahman YILMAZ <15090111061@ybu.edu.tr>
2017-08-06 11:54:03 +0300

    Update lumba.sh

--- lumba.sh
+++ lumba.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install xrdp -y && sudo apt-get install xfce4 -y && echo xfce4-session >~/.xsession && sudo apt-get install firefox -y && clear && sudo service xrdp restart
+sudo apt-get update -y && sudo apt-get install xrdp -y && sudo apt-get install xfce4 -y && echo xfce4-session >~/.xsession && sudo apt-get install firefox -y && clear && sudo service xrdp restart
 echo 
 echo !!!!!!!!!!!!!!!!!!!!!!!!
 echo !! Kurulum tamamlandi.!!
